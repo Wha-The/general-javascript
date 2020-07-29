2defc8d2fbe28cf5a404396a6814aaf55971bd7a
Wha-The <47490607+Wha-The@users.noreply.github.com>
2020-07-29 09:58:43 +0800

    Update function-spy.py

--- function-spy.py
+++ function-spy.py
@@ -4,3 +4,10 @@ def attach(level):
           print(i,v,a,k)
           v(*a**k)
       level[i]=replaceddef
+    
+cmd = {}
+def abc(d,e,f): print(d,e,f)
+cmd.abc = abc
+
+attach(cmd)
+cmd.abc("1",2,3)
