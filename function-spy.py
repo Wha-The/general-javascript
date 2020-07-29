101f404289bdf9b1d5054a66c98fb87210337706
Wha-The <47490607+Wha-The@users.noreply.github.com>
2020-07-29 10:08:43 +0800

    Update function-spy.py

--- function-spy.py
+++ function-spy.py
@@ -2,8 +2,10 @@ class Event():
   def __init__(self,dispatchevent):
     self.event = dispatchevent
   def __call__(self,*a,**k):
-    print(a,k,self.dispatchevent)
-    return self.dispatchevent(a,k)
+    print(a,k,self.event)
+    return self.dispatchevent(self.event,a,k)
+  def dispatchevent(self,evt,a,k):
+    return evt(*a,**k)
 
 def attach(level):
   for i,v in level.items():
