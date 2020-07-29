2e8ebf5ab4b5c51b33372199edbc8cf5744d5ddf
Wha-The <47490607+Wha-The@users.noreply.github.com>
2020-07-29 09:41:25 +0800

    Update function-spy.js

--- function-spy.js
+++ function-spy.js
@@ -1,12 +1,13 @@
 
 function attach(level){
+  var softcopy = level
   for (var key in level) {
       // check if the property/key is defined in the object itself, not in parent
       if (level.hasOwnProperty(key) && key !="attach") {     
           var value = level[key]
           if (typeof value == "function"){
               level[key]=function(){
-                      value.apply(this,arguments)
+                      softcopy[key].apply(this,arguments)
                     }
           }
       }
