f44978732b29c43a5dd4e365c5060b8f702d1e95
Wha-The <47490607+Wha-The@users.noreply.github.com>
2020-07-30 11:26:49 +0800

    Update function-spy.py

--- function-spy.py
+++ function-spy.py
@@ -5,22 +5,22 @@ NOREPEATS = [
 ]
 
 class Event():
-  def __init__(self,dispatchevent):
+  def __init__(self,dispatchevent,dispatcheventname):
     self.event = dispatchevent
+    self.eventname = dispatcheventname
   def __call__(self,*a,**k):
-    print(a,k,self.event)
+    print "Function *"+self.eventname+"* called, arguments: ("+','.join([str(i) for i in a])+") , kwargs: ",k
     return self.dispatchevent(self.event,a,k)
   def dispatchevent(self,evt,a,k):
     return evt(*a,**k)
 
 def attach(level):
   for i,v in level.items():
-      if callable(i) and i not in NOREPEATS:
-         level[i]=Event(v)
+      if callable(v) and i not in NOREPEATS:
+        level[i]=Event(v,i)
   return level
   
-def abc(d):print(d+5)  
-  
-globals() = attach(globals())
+def abc(d):return d+5 
+attach(globals())
 
-abc(5)
+print(abc(5))
